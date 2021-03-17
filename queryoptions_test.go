5db0635015c47d249f8eaff1f19aaa66c943a007
Joshua Thomas <joshuathomas@clearchannel.com>
2021-03-17 11:27:26 -0700

    adding examples... unable to debug on M1

--- queryoptions_test.go
+++ queryoptions_test.go
@@ -17,32 +17,37 @@ func TestFromQuerystring(t *testing.T) {
 		args    args
 		want    Options
 		wantErr bool
-	}{
-		{"empty querystring", args{qs: ""}, Options{}, false},
-		{"multiple filters not repeated", args{qs: "filter[fieldA]=value1&filter[fieldB]=value2"}, Options{
-			Filter: map[string][]string{"fieldA": {"value1"}, "fieldB": {"value2"}},
-			Page:   map[string]int{},
-			Sort:   []string{},
-		}, false},
-		{"multiple filters not repeated and page", args{qs: "filter[fieldA]=value1&filter[fieldB]=value2&page[offset]=100"}, Options{
-			Filter: map[string][]string{"fieldA": {"value1"}, "fieldB": {"value2"}},
-			Page:   map[string]int{"offset": 100},
-			Sort:   []string{},
-		}, false},
-		{"single sort", args{qs: "sort=fieldA"}, Options{
-			Filter: map[string][]string{},
-			Page:   map[string]int{},
-			Sort:   []string{"fieldA"},
-		}, false},
-		{"multiple sort parameters", args{qs: "sort=fieldA&sort=fieldB&sort=fieldC"}, Options{
-			Filter: map[string][]string{},
-			Page:   map[string]int{},
-			Sort:   []string{"fieldA", "fieldB", "fieldC"},
-		}, false},
-		{"mulitple fields via one parameter", args{qs: "sort=fieldA,fieldB,fieldC"}, Options{
-			Filter: map[string][]string{},
-			Page:   map[string]int{},
-			Sort:   []string{"fieldA", "fieldB", "fieldC"},
+	}{ /*
+			{"empty querystring", args{qs: ""}, Options{}, false},
+			{"multiple filters not repeated", args{qs: "filter[fieldA]=value1&filter[fieldB]=value2"}, Options{
+				Filter: map[string][]string{"fieldA": {"value1"}, "fieldB": {"value2"}},
+				Page:   map[string]int{},
+				Sort:   []string{},
+			}, false},
+			{"multiple filters not repeated and page", args{qs: "filter[fieldA]=value1&filter[fieldB]=value2&page[offset]=100"}, Options{
+				Filter: map[string][]string{"fieldA": {"value1"}, "fieldB": {"value2"}},
+				Page:   map[string]int{"offset": 100},
+				Sort:   []string{},
+			}, false},
+			{"single sort", args{qs: "sort=fieldA"}, Options{
+				Filter: map[string][]string{},
+				Page:   map[string]int{},
+				Sort:   []string{"fieldA"},
+			}, false},
+			{"multiple sort parameters", args{qs: "sort=fieldA&sort=fieldB&sort=fieldC"}, Options{
+				Filter: map[string][]string{},
+				Page:   map[string]int{},
+				Sort:   []string{"fieldA", "fieldB", "fieldC"},
+			}, false},
+			{"mulitple fields via one parameter", args{qs: "sort=fieldA,fieldB,fieldC"}, Options{
+				Filter: map[string][]string{},
+				Page:   map[string]int{},
+				Sort:   []string{"fieldA", "fieldB", "fieldC"},
+			}, false},*/
+		{"filters, pagination and sorting provided", args{qs: "filter[fieldA]=value1,value2&filter[fieldB]=*test&page[offset]=10&page[limit]=10&sort=-fieldA,fieldB"}, Options{
+			Filter: map[string][]string{"fieldA": {"value1", "value2"}, "fieldB": {"*test"}},
+			Page:   map[string]int{"offset": 10, "limit": 10},
+			Sort:   []string{"-fieldA", "fieldB"},
 		}, false},
 	}
 	for _, tt := range tests {
